13ade22d3d2090a1ea8795b3e9dc3be79b9c22ad
Patrick Chan <q_p18@hotmail.com>
2020-11-27 15:30:32 +0000

    Review PDtracker.js by Patrick

--- ontrac/src/components/PDtracker.js
+++ ontrac/src/components/PDtracker.js
@@ -12,36 +12,36 @@ const PDTracker = () => {
       title: "#",
       dataIndex: "student_id",
       key: "student_id",
-      // render: (text) => <a href="http://google.co.uk">{text}</a>,
+      // render: (text) => <a href="http://google.co.uk">{text}</a>, // CAN WE REMOVE THIS COMMENT?
     },
     {
       title: "Name",
       dataIndex: "name",
       key: "name",
-      render: (text) => <a href="http://google.co.uk">{text}</a>,
+      render: (text) => <a href="http://google.co.uk">{text}</a>, // DO WE NEED THIS COMMA?
     },
     {
       title: "Communication",
       dataIndex: "Communication",
-      key: "Communication",
+      key: "Communication", // DO WE NEED THIS COMMA?
     },
     {
       title: "Resilience",
       dataIndex: "Resilience",
-      key: "Resilience",
+      key: "Resilience", // DO WE NEED THIS COMMA?
     },
     {
       title: "Growth",
       dataIndex: "Growth",
-      key: "Growth",
+      key: "Growth", // DO WE NEED THIS COMMA?
     },
     {
       title: "Homework Handed In",
       dataIndex: "homework_submitted",
       key: "homework_submitted",
-      // render: (text) => <a href="http://google.co.uk">{text}</a>,
+      // render: (text) => <a href="http://google.co.uk">{text}</a>, // CAN WE REMOVE THIS COMMENT?
 
-      render: (homework_submitted) => (
+      render: (homework_submitted) => ( // CAN WE REMOVE <> and </> ON LINES 45 AND 57, DOES IT DO ANYTHING?
         <>
           {homework_submitted.map((hwk) => {
             let color = hwk === "yes" ? "geekblue" : "green";
@@ -55,8 +55,8 @@ const PDTracker = () => {
             );
           })}
         </>
-      ),
-    },
+      ), // DO WE NEED THIS COMMA?
+    }, // CAN WE REMOVE BELOW CODE WHICH IS COMMENTED OUT ANYWAY?
     // {
     //   title: "Progress",
     //   key: "progress",
@@ -83,11 +83,11 @@ const PDTracker = () => {
       render: (email, record) => (
         <Space size="middle">
           <a href="mailto:{email}">Send {record.name} email</a>
-          {/* <a href="http://google.co.uk">Delete</a> */}
+          {/* <a href="http://google.co.uk">Delete</a> */} 
         </Space>
-      ),
-    },
-  ];
+      ), // DO WE NEED THIS COMMA?
+    }, // DO WE NEED THIS COMMA?
+  ]; // DO WE NEED LINE 86?
 
   return <Table columns={columns} dataSource={students} />;
 };
